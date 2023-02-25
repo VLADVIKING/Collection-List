8274c6d29a08cd1b5edf2f73c8a63529dd82d029
Vlad Khoteenkov <vla_kho@mail.ru>
2023-02-25 14:42:34 +0300

    Methods added

--- src/Main.java
+++ src/Main.java
@@ -21,49 +21,28 @@ public class Main {
             int reqOperation = Integer.parseInt(input);
             switch (reqOperation) {
                 case 1:
-                    System.out.println("Какую покупку хотите добавить?");
-                    String input1 = scanner.nextLine();
-                    shopList.add(input1);
-                    int shopQuantity = shopList.indexOf(input1);
-                    System.out.println("Итого в списке покупок: " + (shopQuantity + 1));
+                    addProduct(scanner, shopList);
                     break;
                 case 2:
                     printShopList(shopList);
                     break;
                 case 3:
                     printShopList(shopList);
-                    System.out.println("Какую покупку хотите удалить? Введите номер или название");
-
-                    String input2 = scanner.nextLine();
-                    boolean isExist = shopList.contains(input2);
-                    if (isExist) {
-                        shopList.remove(input2);
-                        System.out.println("Покупка " + input2 + " удалена,");
-                        printShopList(shopList);
-                        break;
-                    } else {
-                        int numberRemove = Integer.parseInt(input2);
-                        for (int i = 0; i < shopList.size(); i++) {
-                            if ((numberRemove - 1) == i) {
-                                System.out.println("Покупка " + shopList.remove(i) + " удалена,");
-                            }
-                        }
-                    }
+                    if (removeProduct(scanner, shopList)) break;
                     printShopList(shopList);
                     break;
                 case 4:
-                    System.out.println("Введите текст для поиска:");
-                    String input3 = scanner.nextLine();
-                    String queryLower = input3.toLowerCase();
-                    System.out.println("Найдено:");
-                    for (int i = 0; i < shopList.size(); i++) {
-                        String itemLower = shopList.get(i).toLowerCase();
-                        if (itemLower.contains(queryLower)) {
-                            System.out.println((i + 1) + ".  " + shopList.get(i));
-                        }
-                    }
+                    searchProduct(scanner, shopList);
             }
         }
+    }
+
+    private static void addProduct(Scanner scanner, List<String> shopList) {
+        System.out.println("Какую покупку хотите добавить?");
+        String input1 = scanner.nextLine();
+        shopList.add(input1);
+        int shopQuantity = shopList.indexOf(input1);
+        System.out.println("Итого в списке покупок: " + (shopQuantity + 1));
     }
 
     public static void printShopList(List shopList) {
@@ -73,4 +52,39 @@ public class Main {
         }
     }
 
+    private static boolean removeProduct(Scanner scanner, List<String> shopList) {
+        System.out.println("Какую покупку хотите удалить? Введите номер или название");
+        String input2 = scanner.nextLine();
+        boolean isExist = shopList.contains(input2);
+        if (isExist) {
+            shopList.remove(input2);
+            System.out.println("Покупка " + input2 + " удалена,");
+            printShopList(shopList);
+            return true;
+        } else {
+            int numberRemove = Integer.parseInt(input2);
+            for (int i = 0; i < shopList.size(); i++) {
+                if ((numberRemove - 1) == i) {
+                    System.out.println("Покупка " + shopList.remove(i) + " удалена,");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void searchProduct(Scanner scanner, List<String> shopList) {
+        System.out.println("Введите текст для поиска:");
+        String input3 = scanner.nextLine();
+        String queryLower = input3.toLowerCase();
+        System.out.println("Найдено:");
+        for (int i = 0; i < shopList.size(); i++) {
+            String itemLower = shopList.get(i).toLowerCase();
+            if (itemLower.contains(queryLower)) {
+                System.out.println((i + 1) + ".  " + shopList.get(i));
+                break;
+            }
+        }
+        System.out.println("Ничего не найдено!");
+    }
 }
